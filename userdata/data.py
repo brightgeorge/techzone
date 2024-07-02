d682c3276a15ce48e114159ab91dd9e22a3bbb3d
brightgeorge <brightgeorge66@gmail.com>
2024-07-03 00:03:48 +0530

    up0207202412

--- userdata/data.py
+++ userdata/data.py
@@ -286,10 +286,10 @@ def qrcode(request,id):
 
 
 import csv
-def getfile(request):
+def getfile(request,id):
     response = HttpResponse(content_type='text/csv')
     response['Content-Disposition'] = 'attachment; filename="file.csv"'
-    employees = profile.objects.all().filter(id=16)
+    employees = profile.objects.all().filter(id=id)
     writer = csv.writer(response)
     for employee in employees:
         writer.writerow([employee.name,employee.mob,employee.email])
